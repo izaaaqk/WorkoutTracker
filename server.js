b0c1713fb3254c6c38a3db855fdc878a32fbcf27
Isaac Aguirre <isaac.aguirre@du.edu>
2021-09-06 00:31:26 -0600

    deploying

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }
+);
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+app.listen(PORT, ()=> {
+    console.log(`App live on ${PORT}!`)
+});
