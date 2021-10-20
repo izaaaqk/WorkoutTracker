de4ff70de8e180af5e508cc4c7960c30acb7513d
Isaac Aguirre <isaac.aguirre@du.edu>
2021-10-20 13:09:39 -0600

    try4

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +14,13 @@ mongoose.connect(
         useFindAndModify: false,
     }
 );
+const uri = "mongodb+srv://Leo-admin:Password123@Cluster0.n9z04.mongodb.net/workout?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 const app = express();
 
