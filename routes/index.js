662374c9cdfef2da88a52a397fc99052e05a9d3f
Isaac Aguirre <isaac.aguirre@du.edu>
2021-10-25 18:22:10 -0600

    Deploy try 1000

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,6 @@ const path = require('path');
 const router = require('express').Router();
 const Workout =require('../models');
 
-router.get('/api/workouts', (req, res) => {
-    Workout.Workout.find({})
-        .then((data) => {
-            res.json(data);
-        })
-        .catch((err) => {
-            res.stats(400).json(err);
-        });
-});
 
 router.post('/api/workouts', (req, res) => {
     Workout.Workout.create({})
@@ -34,6 +25,7 @@ router.put('/api/workouts/:id', (req, res) => {
         });
 });
 
+
 router.put('/api/workout/:id', (req, res) => {
     const id = req.params.id;
     const body = req.body;
@@ -47,6 +39,16 @@ router.put('/api/workout/:id', (req, res) => {
             })
         .catch((err) => {
             res.status(400).json(err);
+        });
+});
+
+router.get('/api/workouts', (req, res) => {
+    Workout.Workout.find({})
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.stats(400).json(err);
         });
 });
 
